23c06bc90e940d91fcc6ec42755be62c54717a0c
deep-wu <wuzz@hfuu.edu.cn>
2026-04-06 17:02:25 +0800

    Delete NRFP directory

--- NRFP/office31.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gpu=0
-
-for domain in w2a w2d d2w d2a a2w a2d
-do
-    #python pfc_source.py --dset $domain --gpu_id $gpu --office31
-    python pfc_target.py --office31 --dset $domain --gpu_id $gpu
-done
